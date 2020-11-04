dcd44764911236ec5378bc5ade42b6fe6fc95a6c
jeremyvrxd <jeremyvrxd@hotmail.com>
2020-11-04 14:00:07 -0500

    commitxd

---
+++ Sesión 19/js/javascript.js
@@ -0,0 +1,15 @@
+function compara(){
+    var a = 2;
+    var b = 2;
+    if(a == b)
+        alert("a y b son iguales")
+    else
+        alert("a y b son diferentes")
+}
+
+
+
+function resta(x , y){
+    var rest = x - y ;
+    document.write("<h2> La resta es " + rest + "</h2>");
+}
